47ea48f622207329b89577369dcfc8ab04a2972d
IvashkaVIP <86344094+IvashkaVIP@users.noreply.github.com>
2023-07-23 21:40:23 +0000

    Deploying to gh-pages from @ IvashkaVIP/goit-react-hw-05-movies@14bf6d4b0f412ace29599b74d28b98a8977350bb 🚀

---
+++ static/js/357.6526f3d2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[357],{357:function(e,t,o){o.r(t);var s=o(689),c=o(791),i=o(184);t.default=function(){var e=(0,s.UO)().movieId;return console.log("MovieDetails >>> Reviews >>> ",e),(0,c.useEffect)((function(){}),[]),(0,i.jsxs)("div",{children:["Reviews ",e]})}}}]);
+//# sourceMappingURL=357.6526f3d2.chunk.js.map
