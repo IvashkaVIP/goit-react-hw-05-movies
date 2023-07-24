a0fb63130d9483962f47834408d7b44fc4e6cb50
IvashkaVIP <ivashka.co@gmail.com>
2023-07-24 06:51:23 +0300

    Update MovesListItem.js

--- src/components/MovesListItem/MovesListItem.js
+++ src/components/MovesListItem/MovesListItem.js
@@ -1,8 +1,19 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const MoviesListItem = ({ title, name, id }) => {
-    // console.log(title)
-    return <li>{title || name}</li>
+    const location = useLocation();
+    const currentPage =
+      location.pathname === '/' ? 'movies' : location.pathname;
+  return (
+    <li>
+      <Link to={`${currentPage}/${id}`} state={{ from: location }}>
+        {title ?? name}
+      </Link>
+    </li>
+  );
+    
 }
 
 export default MoviesListItem;
